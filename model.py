0f1b92a9697762f7dea6d9553ad23e485b1380ef
unknown <kamil.nkm@mail.ru>
2025-10-20 02:18:32 +0500

    Были добавлены model.py, todo.py, api.py

--- model.py
+++ model.py
@@ -1,5 +1,37 @@
 from pydantic import BaseModel
+from typing import List
 
 class Todo(BaseModel):
     id: int
     item: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "id": 1,
+                "item": "Example Schema!"
+            }
+        }
+
+class TodoItem(BaseModel):
+    item: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "item": "Make music"
+            }
+        }
+
+class TodoItems(BaseModel):
+    todos: List[TodoItem]
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "todos": [
+                    {"item": "Nasyrov"},
+                    {"item": "IVT-430B"}
+                ]
+            }
+        }
